014bc5cdb184204d7577ffe29ec84fd3c7fc1ab0
aditigandhi <agandhi@ucsd.edu>
2021-10-27 20:53:03 -0700

    Finished Login Issue
    Creating user, logging in, remain logged in, and logging out.

--- KeepFresh/GroceriesViewController.swift
+++ KeepFresh/GroceriesViewController.swift
@@ -6,9 +6,19 @@
 //
 
 import UIKit
+import Parse
 
 class GroceriesViewController: UIViewController {
-
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let delegate = windowScene.delegate as? SceneDelegate else {return}
+        
+        delegate.window?.rootViewController = loginViewController
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
